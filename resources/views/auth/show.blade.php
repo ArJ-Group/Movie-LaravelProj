6c3024e3b66402d787c9bcc7dfb6cdb19eb2b68d
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-05-17 10:30:56 +0700

    ADD

--- resources/views/auth/show.blade.php
+++ resources/views/auth/show.blade.php
@@ -40,20 +40,18 @@ var check = function() {
 
 @section('content')
 
-<form method="POST" action="{{ route('user.store') }}" enctype="multipart/form-data">
-    {{ csrf_field() }}
 <div class="row">
             <div class="col-md-12 d-flex align-items-stretch grid-margin">
               <div class="row flex-grow">
                 <div class="col-12">
                   <div class="card">
                     <div class="card-body">
-                      <h4 class="card-title">Tambah user baru</h4>
+                      <h4 class="card-title">Detail <b>{{$data->username}}</b></h4>
                       
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ $data->name }}" required readonly>
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
@@ -64,7 +62,7 @@ var check = function() {
                         <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
                             <label for="username" class="col-md-4 control-label">Username</label>
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required>
+                                <input id="username" type="text" class="form-control" name="username" value="{{ $data->username }}" required readonly="">
                                 @if ($errors->has('username'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('username') }}</strong>
@@ -76,7 +74,7 @@ var check = function() {
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $data->email }}" required readonly>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -88,48 +86,20 @@ var check = function() {
                         <div class="form-group">
                             <label for="email" class="col-md-4 control-label">Gambar</label>
                             <div class="col-md-6">
-                                <img class="product" width="200" height="200" />
-                                <input type="file" class="uploads form-control" style="margin-top: 20px;" name="gambar">
+                                <img class="product" width="200" height="200" @if($data->gambar) src="{{ asset('images/user/'.$data->gambar) }}" @endif />
                             </div>
                         </div>
-
-
-                        <div class="form-group{{ $errors->has('level') ? ' has-error' : '' }}">
+                        @if(Auth::user()->level == 'admin')
+                         <div class="form-group{{ $errors->has('level') ? ' has-error' : '' }}">
                             <label for="level" class="col-md-4 control-label">Level</label>
                             <div class="col-md-6">
-                            <select class="form-control" name="level" required="">
-                                <option value=""></option>
-                                <option value="admin">Admin</option>
-                                <option value="user">User</option>
+                            <select class="form-control" name="level" required="" readonly>
+                                <option value="admin" @if($data->level == 'admin') selected @endif>Admin</option>
+                                <option value="user" @if($data->level == 'user') selected @endif>User</option>
                             </select>
                             </div>
                         </div>
-
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" onkeyup='check();' name="password" required>
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="confirm_password" type="password" onkeyup="check()" class="form-control" name="password_confirmation" required>
-                                <span id='message'></span>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary" id="submit">
-                                    Register
-                        </button>
-                        <button type="reset" class="btn btn-danger">
-                                    Reset
-                        </button>
+                        @endif
                         <a href="{{route('user.index')}}" class="btn btn-light pull-right">Back</a>
                     </div>
                   </div>
@@ -138,5 +108,4 @@ var check = function() {
             </div>
 
 </div>
-</form>
 @endsection
