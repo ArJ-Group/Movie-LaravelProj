20bd78ab26934d15b607ba89ed9f46c9b0de110e
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-05-17 10:56:44 +0700

    ADD

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('member', 'MemberController');
+Route::resource('user', 'UserController');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index');
